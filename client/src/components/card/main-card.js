971255e433ae56251d670ddf07ef0d6d62cd37a7
hzoou <woojooo52@naver.com>
2019-09-19 19:19:31 +0900

    Add main card UI

---
+++ client/src/components/card/main-card.js
@@ -0,0 +1,36 @@
+import MainCardData from "../../data/main-card.js";
+
+class MainCard {
+    constructor() {
+        this.data = MainCardData;
+    }
+
+    render() {
+        return `
+                <div class="main-card">
+                    <div class="card ship clicked rightShadow">
+                        <div class="card-title">Ship</div>
+                        <div class="card-indicator-container selected"><div class="card-indicators"></div></div>
+                    </div>
+                    <div class="card stream">
+                        <div class="card-title">Stream</div>
+                        <div class="card-indicator-container"><div class="card-indicators"></div></div>
+                    </div>
+                    <div class="card shop">
+                        <div class="card-title">Shop</div>
+                        <div class="card-indicator-container"><div class="card-indicators"></div></div>
+                    </div>
+                    <div class="card read">
+                        <div class="card-title">Read</div>
+                        <div class="card-indicator-container"><div class="card-indicators"></div></div>
+                    </div>
+                    <div class="card more">
+                        <div class="card-title">More</div>
+                        <div class="card-indicator-container"><div class="card-indicators"></div></div>
+                    </div>
+                </div>
+                `
+    }
+}
+
+export default MainCard;
