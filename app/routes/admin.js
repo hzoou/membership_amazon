3e70576cae4d0a7007e126311f36ad9f255a9ce7
hzoou <woojooo52@naver.com>
2019-09-24 20:47:11 +0900

    Add api to delete data in table

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql-ssh');
+const multer = require('multer');
+const upload = multer({ dest: './app/public/static_root/' });
 
 router.get('/', function(req, res) {
     res.render('admin.html');
@@ -31,6 +33,32 @@ router.get('/:table', function(req, res) {
             mysql.close();
             result['data'] = results;
             res.send(result);
+        })
+    }).catch(err => {
+        console.log(err)
+    });
+});
+
+router.delete('/:table/:idx', function(req, res) {
+    const table = req.params.table;
+    const idx = req.params.idx;
+    mysql.connect(
+        {
+            host: '106.10.56.165',
+            user: 'hzoou',
+            password: 'boostcamp2019'
+        },
+        {
+            host: '127.0.0.1',
+            user: 'hzoou',
+            password: 'boostcamp2019',
+            database: 'membership_amazon'
+        }
+    ).then(client => {
+        client.query(`DELETE FROM ${table} WHERE idx=${idx}`, function (err) {
+            if (err) throw err;
+            mysql.close();
+            res.status(200).send({status: 'SUCCESS'});
         })
     }).catch(err => {
         console.log(err)
