19dd41e935bbb25933e01e11bc0eea3012604703
hzoou <woojooo52@naver.com>
2019-09-24 20:47:37 +0900

    Add api to update data in table

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -65,4 +65,33 @@ router.delete('/:table/:idx', function(req, res) {
     });
 });
 
+router.put('/:table/:idx', upload.single('image'), function(req, res){
+    const table = req.params.table;
+    const idx = req.params.idx;
+    let value = '';
+    Object.keys(req.body).forEach((b) => value += `${b}='${req.body[b]}',`);
+    value += `image='${req.file.filename}'`;
+    mysql.connect(
+        {
+            host: '106.10.56.165',
+            user: 'hzoou',
+            password: 'boostcamp2019'
+        },
+        {
+            host: '127.0.0.1',
+            user: 'hzoou',
+            password: 'boostcamp2019',
+            database: 'membership_amazon'
+        }
+    ).then(client => {
+        client.query(`UPDATE ${table} SET ${value} WHERE idx=${idx}`, function (err, results, fields) {
+            if (err) throw err;
+            mysql.close();
+            res.status(200).send({status: 'SUCCESS'});
+        })
+    }).catch(err => {
+        console.log(err)
+    });
+});
+
 module.exports = router;
