f0dcfdea9b83500e9b34bcd87dd997691dbfbe35
hzoou <woojooo52@naver.com>
2019-09-24 20:46:22 +0900

    Add api to read data in table

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql-ssh');
 
 router.get('/', function(req, res) {
     res.render('admin.html');
+});
+
+router.get('/:table', function(req, res) {
+    const table = req.params.table;
+    const result = {};
+    mysql.connect(
+        {
+            host: '106.10.56.165',
+            user: 'hzoou',
+            password: 'boostcamp2019'
+        },
+        {
+            host: '127.0.0.1',
+            user: 'hzoou',
+            password: 'boostcamp2019',
+            database: 'membership_amazon'
+        }
+    ).then(client => {
+        client.query(`SHOW FULL COLUMNS FROM ${table}`, function (err, fields) {
+            if (err) throw err;
+            result['column'] = fields;
+        });
+        client.query(`SELECT * FROM ${table}`, function (err, results) {
+            if (err) throw err;
+            mysql.close();
+            result['data'] = results;
+            res.send(result);
+        })
+    }).catch(err => {
+        console.log(err)
+    });
 });
 
 module.exports = router;
