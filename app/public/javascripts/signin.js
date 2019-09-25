e6e8eb31ace0ecdb294b4227a95d07ccd42c7be6
hzoou <woojooo52@naver.com>
2019-09-25 18:33:24 +0900

    Add signin script
    아이디/비밀번호 입력 확인

---
+++ app/public/javascripts/signin.js
@@ -0,0 +1,55 @@
+const signIn = {
+    init() {
+        this.id = document.querySelector('#id');
+        this.pw = document.querySelector('#pw');
+        this.submitBtn = document.querySelector('#submit');
+        this.attachEvent();
+    },
+
+    validation: {
+        'id': {
+            confirm: false,
+            msg: '아이디를 입력해주세요.'
+        },
+        'pw': {
+            confirm: false,
+            msg: '비밀번호를 입력해주세요.'
+        }
+    },
+
+    elements: {
+        id: '',
+        pw: '',
+        submitBtn: ''
+    },
+
+
+    attachEvent() {
+        console.log(this.id);
+        this.id.addEventListener("blur", this.check.bind(this));
+        this.pw.addEventListener("blur", this.check.bind(this));
+        this.submitBtn.addEventListener("click", this.signIn.bind(this));
+    },
+
+    signIn() {
+        if (Object.values(this.validation).every((v) => v.confirm === true)) {
+            // const body = {
+            //     id: this.id.value,
+            //     pw: this.pw.value
+            // };
+            // const res = await fetchAPI('/signin', 'POST', body);
+            // if (res.status == "FAIL") {
+            //     alert(res.msg);
+            // } else self.location.href = '/mypage';
+        } else {
+            const element = (Object.values(this.validation).find((e) => !e.confirm));
+            alert(element.msg);
+        }
+    },
+
+    check(e) {
+        this.validation[e.target.id].confirm = (!!e.target.value)
+    }
+};
+
+window.addEventListener('DOMContentLoaded', signIn.init());
