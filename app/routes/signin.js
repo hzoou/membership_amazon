15720cec44654c96c33954197ccd32d16a01b833
hzoou <woojooo52@naver.com>
2019-09-27 07:10:46 +0900

    Remove unused comment

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/user');
 
-/* GET home page. */
 router.get('/', function(req, res) {
     res.render('signin.html');
 });
-
-router.post('/', (req, res) => {
-    user.isHaveId(req, res);
-});
-
 
 module.exports = router;
