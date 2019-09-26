5f85b66ae478e63a38f16afbfa3834665594ab2f
hzoou <woojooo52@naver.com>
2019-09-27 04:01:50 +0900

    Modify database config

--- app/database/config.js
+++ app/database/config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    ssh: {
-        host: '106.10.56.165',
-        user: 'hzoou',
-        password: 'boostcamp2019'
-    },
     db: {
         host: '106.10.56.165',
         user: 'hzoou',
--- app/database/connection.js
+++ app/database/connection.js
@@ -1,14 +1,9 @@
-// const mysql = require('mysql-ssh');
 const config = require('./config');
 const mysql = require('mysql2');
 
 
 module.exports = {
     connect: () => {
-        return mysql.createConnection(config.db)
-        // return mysql.connect(config.ssh, config.db);
-    },
-    close: () => {
-        // return mysql.close();
+        return mysql.createConnection(config.db);
     }
 };
